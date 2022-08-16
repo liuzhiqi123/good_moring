d8995fe514298c6deea78a7778cfb4b12a2a9c58
liuzhiqi123 <964965720@qq.com>
2022-08-16 13:42:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ wm = WeChatMessage(client)
 wea, temperature = get_weather()
 
 temMo = temperature + '最高30   最低22'
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":temMo},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
